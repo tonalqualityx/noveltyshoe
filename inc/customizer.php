1334e2b8d3cacff89fdcdb1612f5c3218695478c
tonalqualityx <mike@becomeindelible.com>
2019-04-05 02:33:43 -0400

    initial setup

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Novelty Shoe Theme Customizer
+ *
+ * @package Novelty_Shoe
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function novelty_shoe_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'novelty_shoe_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'novelty_shoe_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'novelty_shoe_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function novelty_shoe_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function novelty_shoe_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function novelty_shoe_customize_preview_js() {
+	wp_enqueue_script( 'novelty-shoe-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'novelty_shoe_customize_preview_js' );
